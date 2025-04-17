81cc0f3f9bab829425a3b19e6621476367845210
Nizyyy55 <Nethmithahasaral909@gmail.com>
2025-04-17 22:38:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&MG9G3RxT#Zz8X2KsnDmPVukQCjEWM5XOs4cRT2spWlvRjjd7tmhg' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
